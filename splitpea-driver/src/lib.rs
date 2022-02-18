54da16829050a5a5ed5336d3bfdd1d23988fc5b5
James Munns <james@onevariable.com>
2022-02-18 19:57:30 +0100

    Works on std/no_std

--- splitpea-driver/src/lib.rs
+++ splitpea-driver/src/lib.rs
@@ -1,9 +1,10 @@
 #![cfg_attr(not(feature = "use-std"), no_std)]
 
-use embedded_hal::blocking::i2c::WriteRead;
 pub use splitpea_icd as icd;
+use embedded_hal::blocking::i2c::WriteRead;
 use core::fmt::Debug;
 
+#[derive(Debug, PartialEq)]
 pub enum Error<T: Debug> {
     I2c(T),
     Postcard(postcard::Error),
@@ -38,15 +39,36 @@ where
         self.i2c.write_read(0x42, &[0x10], &mut buf).map_err(Error::I2c)?;
         Ok(buf[0] as usize)
     }
+
+    // TODO: Magic number
+    pub fn get_all_events(&mut self) -> Result<heapless::Vec<icd::Event, 32>, Error<I2C::Error>> {
+        let ct = self.get_event_cts()?;
+
+        if ct == 0 {
+            return Ok(heapless::Vec::new());
+        }
+
+        let mut resp = heapless::Vec::new();
+        let mut buf = [0u8; 64]; // TODO: Magic number
+        let bufsl = &mut buf [..(2 * ct)];
+        self.i2c.write_read(0x42, &[0x20, ct as u8], bufsl).map_err(Error::I2c)?;
+
+        for ch in bufsl.chunks_exact(2) {
+            let event: icd::Event = postcard::from_bytes(ch)?;
+            resp.push(event).ok();
+        }
+
+        Ok(resp)
+    }
 }
 
-// #[cfg(feature = "use_std")]
+#[cfg(feature = "use-std")]
 impl<I2C> Splitpea<I2C>
 where
     I2C: WriteRead,
     I2C::Error: Debug,
 {
-    pub fn get_all_events(&mut self) -> Result<Vec<icd::Event>, Error<I2C::Error>> {
+    pub fn get_all_events_vec(&mut self) -> Result<Vec<icd::Event>, Error<I2C::Error>> {
         let ct = self.get_event_cts()?;
 
         if ct == 0 {
